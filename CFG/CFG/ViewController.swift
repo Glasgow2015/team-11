89a6e76eb75f1ec9c67deb16e84f80bfc9a7e474
mananchai <mananchair@gmail.com>
2015-11-07 21:25:42 +0000

    commmmmittttt

--- CFG/CFG/ViewController.swift
+++ CFG/CFG/ViewController.swift
@@ -8,12 +8,36 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+    @IBOutlet weak var imageView: UIImageView!
 
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+       
+        
     }
+    
+    @IBAction func getImage(sender: AnyObject) {
+        var img = UIImagePickerController()
+        img.delegate = self
+        img.allowsEditing = false;
+        
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera){
+            img.sourceType = UIImagePickerControllerSourceType.Camera
+        }else{
+            img.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
+        }
+        
+        self.presentViewController(img, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingImage image: UIImage, editingInfo: [String : AnyObject]?) {
+        <#code#>
+    }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
